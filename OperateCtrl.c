78c1f5f6c509ce72204dd47df3fd6658a04acf5d
lizhenguo <lizhenguo2014@163.com>
2018-05-22 23:17:20 +0800

    增加控制模块文件OperateCtrl.h和OperateCtrl.c By lizhenguo

---
+++ OperateCtrl.c
@@ -0,0 +1,17 @@
+#include "OperateCtrl.h"
+void OperateCtrlInit(void)
+{
+	
+}
+void OperateCtrl(UINT8 type,BOOL operate)
+{
+
+}
+BOOL OperateState(UINT8 type,UINT8* result)
+{
+
+}
+void OperateRetry(void)
+{
+
+}
---
+++ OperateCtrl.h
@@ -0,0 +1,21 @@
+#ifndef _OPERATE_CTRL_H_
+#define _OPERATE_CTRL_H_
+#include "BasicDef.h"
+typedef enum{
+	OUT_PORT_1=0,
+	OUT_PORT_2,
+	OUT_PORT_3,
+	OUT_PORT_4,
+	OUT_PORT_MAX,
+}OUTPORTLIST;
+typedef enum{
+	IN_PORT_1=0,
+	IN_PORT_2,
+	IN_PORT_3,
+	IN_PORT_MAX,
+}INPORTLIST;
+extern void OperateCtrl(UINT8 type,BOOL operate);
+extern BOOL OperateState(UINT8 type,UINT8* result);
+extern void OperateRetry(void);
+extern void OperateCtrlInit(void);
+#endif
